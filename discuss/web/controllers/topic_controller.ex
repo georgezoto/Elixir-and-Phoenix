0ffad6fa3c923c38b283c04b95d8cf1f153b4d4f
georgezoto <gezotos@gmail.com>
2017-07-25 21:45:48 -0400

    Modify the topic controller, add a topic template along with a topic view.

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -1,12 +1,15 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
+  alias Discuss.Topic
 
   def new(conn, params) do
-    IO.puts "++++"
-    IO.inspect conn
-    IO.puts "++++"
-    IO.inspect params
-
+    #IO.puts "++++"
+    #IO.inspect conn
+    #IO.puts "++++"
+    #IO.inspect params
+    changeset = Topic.changeset(%Topic{}, %{})
+  
+    render conn, "new.html"
   end
   
 end
