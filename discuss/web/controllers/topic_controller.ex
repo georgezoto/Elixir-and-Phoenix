a186ef6038cf3a4954daf06ac633dccc178de240
georgezoto <gezotos@gmail.com>
2017-07-25 23:27:51 -0400

    Modify the topic controller, add a new route.

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -2,14 +2,17 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
   alias Discuss.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     #IO.puts "++++"
     #IO.inspect conn
     #IO.puts "++++"
     #IO.inspect params
     changeset = Topic.changeset(%Topic{}, %{})
   
-    render conn, "new.html"
+    render conn, "new.html", changeset: changeset
   end
-  
+ 
+  def create(conn, %{"topic" => topic}) do
+    topic
+  end 
 end
