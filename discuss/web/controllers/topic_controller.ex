3d51b5a791bbb2d07f8fd226ce9e0000ae91d863
georgezoto <gezotos@gmail.com>
2017-08-02 23:56:36 -0400

    Add edit function in topic contoller to edit a topic.

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -17,17 +17,11 @@ defmodule Discuss.TopicController do
     render conn, "new.html", changeset: changeset
   end
  
-  def create(conn, %{"topic" => topic}) do
-    changeset = Topic.changeset(%Topic{}, topic)
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
 
-    case Repo.insert(changeset) do
-      {:ok, post} -> 
-        conn
-        |> put_flash(:info, "Topic Created")
-        |> redirect(to: topic_path(conn, :index)) 
-      {:error, changeset} -> 
-        render conn, "new.html", changeset: changeset
-    end
+    render conn, "edit.html", changeset: changeset, topic: topic
   end
  
 end
