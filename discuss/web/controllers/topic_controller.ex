b5eb2fbb5033830f3d036b9138e440f95faf34a1
georgezoto <gezotos@gmail.com>
2017-08-02 23:59:33 -0400

    Correct topic contoller to edit a topic.

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -17,6 +17,19 @@ defmodule Discuss.TopicController do
     render conn, "new.html", changeset: changeset
   end
  
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> 
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index)) 
+      {:error, changeset} -> 
+        render conn, "new.html", changeset: changeset
+    end
+  end
+
   def edit(conn, %{"id" => topic_id}) do
     topic = Repo.get(Topic, topic_id)
     changeset = Topic.changeset(topic)
