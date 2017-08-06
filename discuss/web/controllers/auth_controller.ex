c09c2acbf922bff5991e567b36e8cad6581ab961
georgezoto <gezotos@gmail.com>
2017-08-06 19:48:41 -0400

    Extract user information from github auth message.

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -1,12 +1,15 @@
 defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
+  alias Discuss.User
 
-  def callback(conn, params) do
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
     #IO.inspect(conn.assigns)
     #IO.inspect(params)
+    IO.inspect(params)
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
 
-    
   end
 
 end
