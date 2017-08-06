ecb824f030cfa0eabe97e229e0f6cef42bfb15b5
georgezoto <gezotos@gmail.com>
2017-08-05 20:28:26 -0400

    Update .gitignore

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -34,3 +34,4 @@ config :ueberauth, Ueberauth,
 config :ueberauth, Ueberauth.Strategy.Github.OAuth,
   client_id: System.get_env("GITHUB_CLIENT_ID"),
   client_secret: System.get_env("GITHUB_CLIENT_SECRET")
+#Test
