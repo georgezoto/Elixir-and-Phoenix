09561b3b16bfb35a89bc4c11ea53e4d8492abf99
georgezoto <gezotos@gmail.com>
2017-08-06 20:17:30 -0400

    Add logic to create a newly authenticated user in the backend db.

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -10,6 +10,17 @@ defmodule Discuss.AuthController do
     user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
     changeset = User.changeset(%User{}, user_params)
 
+    insert_or_update_user(changeset)
+  end
+
+  #A private function that inserts or updates a user record based on his email
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
   end
 
 end
