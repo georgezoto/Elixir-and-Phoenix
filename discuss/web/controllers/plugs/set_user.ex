72d69b239a1973b394292c1827d130cd63c21cc6
georgezoto <gezotos@gmail.com>
2017-08-07 20:46:53 -0400

    Add a plug module to assign a user id to connection conn.

---
+++ discuss/web/controllers/plugs/set_user.ex
@@ -0,0 +1,20 @@
+defmodule Discuss.Plugs.SetUser do
+  import Plug.Conn
+  import Phoenix.Controller
+
+  alias Discuss.Repo
+  alias Discuss.User
+  alias Discuss.Router.Helpers
+
+  def init(_params) do
+
+  end
+
+  def call(conn, _params) do
+    user_id = get_session(conn, :user_id)
+
+
+
+  end
+
+end
