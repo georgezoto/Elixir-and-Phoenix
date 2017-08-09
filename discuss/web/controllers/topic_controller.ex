c129906cd9fe6c2e304906ba5ac97a64aa9cfc00
georgezoto <gezotos@gmail.com>
2017-08-09 00:32:13 -0400

    Hide Edit and Delete links if a user does not own them. Protect edit/2, update/2, delete/2 from other users by using a function plug.

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -4,6 +4,8 @@ defmodule Discuss.TopicController do
 
   #Flow through the RequireAuth plug prior to these actions using the guard clause
   plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  #A function plug to check topic ownership
+  plug :check_topic_owner when action in [:edit, :update, :delete]
 
   def index(conn, _params) do
     IO.inspect(conn.assigns)
@@ -88,6 +90,19 @@ defmodule Discuss.TopicController do
     conn
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You cannot edit that")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
   end
 
 end
