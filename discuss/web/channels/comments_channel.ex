86fb94d8ee0f020c8c34e6f72820fdee2ca3ecb3
georgezoto <gezotos@gmail.com>
2017-08-26 23:00:45 -0400

    Update comments_channel.

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -1,10 +1,14 @@
 defmodule Discuss.CommentsChannel do
   use Phoenix.Channel
 
-  def join("topic:comments", _message, socket) do
-    {:ok, socket}
+  #"topic:comments"
+  def join(name, _message, socket) do
+    IO.puts("+++++++++++++++++++++++++")
+    IO.puts(name)
+    {:ok, %{my_header: my_reply}, socket}
   end
 
+  #Return error for unauthorized topics
   def join("topic:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
   end
