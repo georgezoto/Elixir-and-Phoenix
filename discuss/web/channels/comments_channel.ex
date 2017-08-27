929f6b8fc0b15dbff99b875e63af579a47472878
georgezoto <gezotos@gmail.com>
2017-08-27 18:21:57 -0400

    Update comments_channel.

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -1,17 +1,28 @@
 defmodule Discuss.CommentsChannel do
-  use Phoenix.Channel
+  use Discuss.Web, :channel
+  alias Discuss.Topic
 
   #To authorize the socket to join a topic, we return {:ok, socket} or {:ok, reply, socket}.
   #"topic:subtopic"
-  def join(name, _message, socket) do
-    IO.puts("\n\ndef join(name, _message, socket) +++++++++++")
-    IO.puts(name)
-    {:ok, %{my_header: "my_reply"}, socket}
-  end
+  #def join(name, _message, socket) do
+  #  IO.puts("\n\ndef join(name, _message, socket) +++++++++++")
+  #  IO.puts(name)
+  #  {:ok, %{my_header: "my_reply"}, socket}
+  #end
 
   #To deny access, we return {:error, reply}.
-  def join("comments:" <> _private_room_id, _params, _socket) do
-    {:error, %{reason: "unauthorized"}}
+  #def join("comments:" <> _private_room_id, _params, _socket) do
+  #  {:error, %{reason: "unauthorized"}}
+  #end
+
+  def join("comments:" <> topic_id, _params, socket) do
+    IO.puts("\n\ndef join/3 +++++++++++")
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+    socket = assign(socket, :topic, topic)
+    IO.inspect(socket)
+
+    {:ok, %{my_header: "my_reply"}, socket}
   end
 
   def handle_in(name, message, socket) do
@@ -19,6 +30,7 @@ defmodule Discuss.CommentsChannel do
     #broadcast! socket, "new_msg", %{body: body}
     IO.puts(name)
     IO.inspect(message)
+    IO.inspect(socket)
 
     {:reply, :ok, socket}
   end
